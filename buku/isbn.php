eaca83e15229b9d0bd940d458495543818cbb513
Delia putri <12050122676@students.uin-suska.ac.id>
2022-06-05 10:37:26 +0700

    first commit

---
+++ buku/isbn.php
@@ -0,0 +1,8 @@
+insert into buku values(
+    "090909",
+    " scared ",
+    "adel",
+    09,
+    "2021-09-09",
+    " what do you mean? you need me to go far away or i should being your clown again ?"
+     );
